2a6382cd106c78e4fc30993e7670a8ec742a82bf
Soldier <1224954468@qq.com>
2023-01-18 21:34:49 +0800

    feat(entity): UserInfo字段调整

--- models/user_info.go
+++ models/user_info.go
@@ -1,8 +1,9 @@
 package models
 
 type UserInfo struct {
-	Id            int    `gorm:"PrimaryKey;AutoIncrement;Unique" json:"id"`
-	Name          string `gorm:"type:varchar(255) not null;" json:"name"`
+	ID            int    `json:"id"`
+	Name          string `gorm:"type:varchar(32) not null;uniqueIndex" json:"name"`
+	Password      string `gorm:"type:varchar(255) not null;"`
 	FollowCount   int    `json:"follow-count"`
 	FollowerCount int    `json:"follower-count"`
 	IsFollow      bool   `json:"is-follow"`
